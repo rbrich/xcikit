f67297e1679703fcc23beff3ea4a54a9be377031
Radek Brich <radek.brich@devl.cz>
2019-05-15 18:25:18 +0200

    [build] Enhanced build.sh - use it also for CircleCI

--- build.sh
+++ build.sh
@@ -1,43 +1,88 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 set -e
 cd $(dirname $0)
-
 ROOT_DIR="$PWD"
 BUILD_TYPE=MinSizeRel
+GENERATOR="Unix Makefiles"
+
+print_usage()
+{
+    echo "Usage: ./build.sh [<phase>, ...] [-G <cmake_generator>]"
+    echo "Where: <phase> = all (default) | deps | config | build | test | install"
+    echo "       <cmake_generator> = \"Unix Makefiles\" | Ninja | ..."
+}
+
+phase()
+{
+    local PHASE="phase_$1"
+    test -n "${!PHASE}" -o -n "${phase_all}"
+}
+
+# parse args...
+phase_all=yes
+while [[ $# > 0 ]] ; do
+    case "$1" in
+        all|deps|config|build|test|install )
+            phase_all=
+            declare "phase_$1=yes"
+            shift 1 ;;
+        -G )
+            GENERATOR="$2"
+            shift 2 ;;
+        * )
+            printf "Error: Unsupported option ${1}.\n\n"
+            print_usage
+            exit 1 ;;
+    esac
+done
+
+echo "=== Settings ==="
+
 BUILD_CONFIG="$(uname)_$(uname -m)_${BUILD_TYPE}"
+[[ "${GENERATOR}" != "Unix Makefiles" ]] && BUILD_CONFIG="${BUILD_CONFIG}_${GENERATOR}"
 BUILD_DIR="${ROOT_DIR}/build/${BUILD_CONFIG}"
 INSTALL_DIR="${ROOT_DIR}/artifacts/${BUILD_CONFIG}"
 
-mkdir -p "${BUILD_DIR}"
-
-echo "=== Settings ==="
+echo "BUILD_CONFIG: ${BUILD_CONFIG}"
 echo "BUILD_DIR:    ${BUILD_DIR}"
 echo "INSTALL_DIR:  ${INSTALL_DIR}"
 echo
 
-echo "=== Install Dependencies ==="
-(cd "${BUILD_DIR}"; conan install "${ROOT_DIR}" \
-    --build missing)
-echo
+mkdir -p "${BUILD_DIR}"
 
-echo "=== Configure ==="
-(cd "${BUILD_DIR}"; cmake "${ROOT_DIR}" \
-    -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
-    -DCMAKE_INSTALL_PREFIX="${INSTALL_DIR}" \
-)
-echo
+if phase deps; then
+    echo "=== Install Dependencies ==="
+    (cd "${BUILD_DIR}"; conan install "${ROOT_DIR}" --build missing)
+    echo
+fi
 
-echo "=== Build ==="
-cmake --build ${BUILD_DIR}
-echo
+if phase config; then
+    echo "=== Configure ==="
+    (cd "${BUILD_DIR}"; cmake "${ROOT_DIR}" \
+        -G "${GENERATOR}" \
+        -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
+        -DCMAKE_INSTALL_PREFIX="${INSTALL_DIR}" \
+    )
+    echo
+fi
 
-echo "=== Test ==="
-cmake --build ${BUILD_DIR} --target test
-echo
+if phase build; then
+    echo "=== Build ==="
+    cmake --build ${BUILD_DIR}
+    echo
+fi
 
-echo "=== Install ==="
-cmake --build ${BUILD_DIR} --target install
-echo
+if phase test; then
+    echo "=== Test ==="
+    cmake --build ${BUILD_DIR} --target test
+    echo
+fi
 
-echo "=== All Done ==="
+if phase install; then
+    echo "=== Install ==="
+    cmake --build ${BUILD_DIR} --target install
+    echo
+fi
 
+echo "=== Done ==="
