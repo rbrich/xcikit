5138127341be42a1ae00e2c37c8e385814f92ae1
Radek Brich <radek.brich@devl.cz>
2020-01-19 18:02:27 +0100

    [test][core] separate dispatch.h test

--- tests/test_core.cpp
+++ tests/test_core.cpp
@@ -6,7 +6,6 @@
 #include <xci/core/format.h>
 #include <xci/core/log.h>
 #include <xci/core/file.h>
-#include <xci/core/dispatch.h>
 #include <xci/core/string.h>
 #include <xci/core/chrono.h>
 #include <xci/core/memory.h>
@@ -14,11 +13,9 @@
 #include <fstream>
 #include <string>
 #include <cstdio>
-#include <unistd.h>
 #include <sys/stat.h>
 
 using namespace xci::core;
-using std::this_thread::sleep_for;
 using namespace std::string_literals;
 
 
@@ -73,55 +70,28 @@ TEST_CASE( "read_binary_file", "[file]" )
 }
 
 
-TEST_CASE( "File watch", "[FSDispatch]" )
+TEST_CASE( "path_dirname", "[file]" )
 {
-    Logger::init(Logger::Level::Error);
-    FSDispatch fw;
+    CHECK(path_dirname("/dir/name/") == "/dir");
+    CHECK(path_dirname("/dir/name") == "/dir");
+    CHECK(path_dirname("name") == ".");
+    CHECK(path_dirname(".") == ".");
+    CHECK(path_dirname("..") == ".");
+    CHECK(path_dirname("/name") == "/");
+#ifdef WIN32
+    CHECK(path_dirname("C:\\xyz\\fsd") == "C:\\xyz");
+    CHECK(path_dirname("C:\\xyz\\") == "C:\\");
+#endif
+}
 
-    std::string tmpname = "/tmp/xci_test_filewatch.XXXXXX";
-    close(mkstemp(&tmpname[0]));
-    std::ofstream f(tmpname);
 
-    FSDispatch::Event expected_events[] = {
-        FSDispatch::Event::Modify,  // one
-        FSDispatch::Event::Modify,  // two
-        FSDispatch::Event::Modify,  // three
-        FSDispatch::Event::Delete,  // unlink
-    };
-    size_t ev_ptr = 0;
-    size_t ev_size = sizeof(expected_events) / sizeof(expected_events[0]);
-    fw.add_watch(tmpname,
-            [&expected_events, &ev_ptr, ev_size] (FSDispatch::Event ev)
-    {
-        CHECK(ev_ptr < ev_size);
-        CHECK(expected_events[ev_ptr] == ev);
-        ev_ptr++;
-    });
-
-    // modify
-    f << "one" << std::endl;
-    sleep_for(50ms);
-
-    // modify, close
-    f << "two" << std::endl;
-    f.close();
-    sleep_for(50ms);
-
-    // reopen, modify, close
-    f.open(tmpname, std::ios::app);
-    f << "three" << std::endl;
-    f.close();
-    sleep_for(50ms);
-
-    // delete
-    ::unlink(tmpname.c_str());
-    sleep_for(50ms);
-
-    // although the inotify watch is removed automatically after delete,
-    // this should still be called to cleanup the callback info
-    fw.remove_watch(tmpname);
-
-    CHECK(ev_ptr == ev_size);  // got all expected events
+TEST_CASE( "path_basename", "[file]" )
+{
+    CHECK(path_basename("/dir/name/") == "name");
+    CHECK(path_basename("/dir/name") == "name");
+    CHECK(path_basename("/name") == "name");
+    CHECK(path_basename("name") == "name");
+    CHECK(path_basename(".") == ".");
 }
 
 
---
+++ tests/test_dispatch.cpp
@@ -0,0 +1,72 @@
+// test_dispatch.cpp created on 2020-01-19 as part of xcikit project
+// https://github.com/rbrich/xcikit
+//
+// Copyright 2020 Radek Brich
+// Licensed under the Apache License, Version 2.0 (see LICENSE file)
+
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+
+#include <xci/core/dispatch.h>
+
+#include <fstream>
+#include <string>
+#include <cstdio>
+#include <unistd.h>
+#include <sys/stat.h>
+
+using namespace xci::core;
+using std::this_thread::sleep_for;
+using namespace std::string_literals;
+
+
+TEST_CASE( "File watch", "[FSDispatch]" )
+{
+    Logger::init(Logger::Level::Error);
+    FSDispatch fw;
+
+    std::string tmpname = "/tmp/xci_test_filewatch.XXXXXX";
+    close(mkstemp(&tmpname[0]));
+    std::ofstream f(tmpname);
+
+    FSDispatch::Event expected_events[] = {
+        FSDispatch::Event::Modify,  // one
+        FSDispatch::Event::Modify,  // two
+        FSDispatch::Event::Modify,  // three
+        FSDispatch::Event::Delete,  // unlink
+    };
+    size_t ev_ptr = 0;
+    size_t ev_size = sizeof(expected_events) / sizeof(expected_events[0]);
+    fw.add_watch(tmpname,
+            [&expected_events, &ev_ptr, ev_size] (FSDispatch::Event ev)
+    {
+        CHECK(ev_ptr < ev_size);
+        CHECK(expected_events[ev_ptr] == ev);
+        ev_ptr++;
+    });
+
+    // modify
+    f << "one" << std::endl;
+    sleep_for(50ms);
+
+    // modify, close
+    f << "two" << std::endl;
+    f.close();
+    sleep_for(50ms);
+
+    // reopen, modify, close
+    f.open(tmpname, std::ios::app);
+    f << "three" << std::endl;
+    f.close();
+    sleep_for(50ms);
+
+    // delete
+    ::unlink(tmpname.c_str());
+    sleep_for(50ms);
+
+    // although the inotify watch is removed automatically after delete,
+    // this should still be called to cleanup the callback info
+    fw.remove_watch(tmpname);
+
+    CHECK(ev_ptr == ev_size);  // got all expected events
+}
