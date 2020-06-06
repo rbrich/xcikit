278755055711a8457caf0b2b59269834fa04599c
Radek Brich <radek.brich@devl.cz>
2020-06-06 10:25:43 +0200

    [build] fix CMake warning: replxx -> Replxx

--- cmake/FindReplxx.cmake
+++ cmake/FindReplxx.cmake
@@ -1,34 +1,34 @@
 # Try to find Replxx.
-# Use replxx_ROOT_DIR to specify custom installation dir.
+# Use Replxx_ROOT_DIR to specify custom installation dir.
 #
 # Once done this will define:
-#   replxx_FOUND
-#   replxx_INCLUDE_DIRS
-#   replxx_LIBRARIES
-#   replxx::replxx target
+#   Replxx_FOUND
+#   Replxx_INCLUDE_DIRS
+#   Replxx_LIBRARIES
+#   Replxx::replxx target
 
-find_path(replxx_INCLUDE_DIR
+find_path(Replxx_INCLUDE_DIR
     NAMES replxx.hxx
-    PATHS ${replxx_ROOT_DIR})
-mark_as_advanced(replxx_INCLUDE_DIR)
+    PATHS ${Replxx_ROOT_DIR})
+mark_as_advanced(Replxx_INCLUDE_DIR)
 
-find_library(replxx_LIBRARY
-    NAMES replxx replxx-static replxx-static-d
-    PATHS ${replxx_ROOT_DIR})
-mark_as_advanced(replxx_LIBRARY)
+find_library(Replxx_LIBRARY
+    NAMES replxx replxx-d replxx-static replxx-static-d
+    PATHS ${Replxx_ROOT_DIR})
+mark_as_advanced(Replxx_LIBRARY)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(replxx DEFAULT_MSG
-    replxx_LIBRARY replxx_INCLUDE_DIR)
+find_package_handle_standard_args(Replxx DEFAULT_MSG
+    Replxx_LIBRARY Replxx_INCLUDE_DIR)
 
-if (replxx_FOUND)
-    set(replxx_LIBRARIES ${replxx_LIBRARY})
-    set(replxx_INCLUDE_DIRS ${replxx_INCLUDE_DIR})
-    if (NOT TARGET replxx::replxx)
-        add_library(replxx::replxx INTERFACE IMPORTED)
-        set_target_properties(replxx::replxx PROPERTIES
-            INTERFACE_LINK_LIBRARIES "${replxx_LIBRARIES}"
-            INTERFACE_INCLUDE_DIRECTORIES "${replxx_INCLUDE_DIRS}"
+if (Replxx_FOUND)
+    set(Replxx_LIBRARIES ${Replxx_LIBRARY})
+    set(Replxx_INCLUDE_DIRS ${Replxx_INCLUDE_DIR})
+    if (NOT TARGET Replxx::replxx)
+        add_library(Replxx::replxx INTERFACE IMPORTED)
+        set_target_properties(Replxx::replxx PROPERTIES
+            INTERFACE_LINK_LIBRARIES "${Replxx_LIBRARIES}"
+            INTERFACE_INCLUDE_DIRECTORIES "${Replxx_INCLUDE_DIRS}"
             INTERFACE_COMPILE_DEFINITIONS "REPLXX_STATIC")
     endif()
 endif()
