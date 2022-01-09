5c0bb0b19418d859e0ba538377bd47ae6b1ad8bb
Radek Brich <radek.brich@devl.cz>
2022-01-09 20:00:50 +0100

    cosmetics

--- src/xci/script/Interpreter.cpp
+++ src/xci/script/Interpreter.cpp
@@ -1,7 +1,7 @@
 // Interpreter.cpp created on 2019-06-21 as part of xcikit project
 // https://github.com/rbrich/xcikit
 //
-// Copyright 2019–2021 Radek Brich
+// Copyright 2019–2022 Radek Brich
 // Licensed under the Apache License, Version 2.0 (see LICENSE file)
 
 #include "Interpreter.h"
--- src/xci/script/ast/AST.cpp
+++ src/xci/script/ast/AST.cpp
@@ -1,7 +1,7 @@
 // AST.cpp created on 2019-05-15 as part of xcikit project
 // https://github.com/rbrich/xcikit
 //
-// Copyright 2019–2021 Radek Brich
+// Copyright 2019–2022 Radek Brich
 // Licensed under the Apache License, Version 2.0 (see LICENSE file)
 
 #include "AST.h"
