50590e36673871d394cf02a1f166ef77daf46d8a
Radek Brich <radek.brich@devl.cz>
2023-01-03 19:16:06 +0100

    [build] cmake: set CMAKE_BUILD_TYPE=Release by default also on Windows/MSVC

--- cmake/XciBuildOptions.cmake
+++ cmake/XciBuildOptions.cmake
@@ -15,13 +15,6 @@ option(FAIL_ON_WARNINGS "Treat warnings as errors. Applies also to clang-tidy." 
 option(ENABLE_TIDY "Run clang-tidy on each compiled file, when available." OFF)
 option(ENABLE_IWYU "Run iwyu (Include What You Use) on each compiled file, when available." OFF)
 
-# default build type + possible values for cmake gui
-if (NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
-    set(CMAKE_BUILD_TYPE "Release" CACHE STRING "Choose the type of build." FORCE)
-endif()
-set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "Debug" "Release" "MinSizeRel" "RelWithDebInfo")
-message(STATUS "Build type: ${CMAKE_BUILD_TYPE}")
-
 # optimizations
 if(CMAKE_BUILD_TYPE STREQUAL "MinSizeRel")
     set(MINSIZE ON)
