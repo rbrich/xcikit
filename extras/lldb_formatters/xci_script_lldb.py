e6d3d8154b28602becf7fde14c55d91f6b70a635
Radek Brich <radek.brich@devl.cz>
2023-08-25 21:05:01 +0200

    [extras] lldb formatter: add summary for TypeInfo, Signature, SymbolPointer, MatchScore

--- extras/lldb_formatters/xci_script_lldb.py
+++ extras/lldb_formatters/xci_script_lldb.py
@@ -30,9 +30,67 @@ class IndexedMap_SyntheticChildrenProvider:
         return elem.Clone("[" + str(index) + "]")
 
 
+def unquote(v):
+    return v.removeprefix('"').removesuffix('"')
+
+
+def TypeInfo_Summary(valobj, _dict, _options):
+    m_type = valobj.GetChildMemberWithName('m_type')
+    m_info = valobj.GetChildMemberWithName('m_info')
+    variant_value = m_info.GetChildAtIndex(0)
+    type_ = m_type.value
+    if type_ == 'Unknown':
+        symidx = variant_value.GetChildMemberWithName('m_symidx').GetValueAsUnsigned()
+        if symidx != 0xffffffff:
+            return variant_value.summary
+        return str(type_)
+    if type_ == 'List':
+        elem_type = variant_value.GetChildAtIndex(0).summary
+        return f"[{elem_type}]"
+    if type_ == 'Tuple':
+        return f"({', '.join(x.summary for x in variant_value.children)})"
+    if type_ == 'Struct':
+        return f"({', '.join('='.join(unquote(p.summary) for p in x.children) for x in variant_value.children)})"
+    if type_ == 'Function':
+        signature = variant_value.GetChildAtIndex(0).Dereference()
+        return signature.summary
+    res = str(type_)
+    if variant_value.GetChildAtIndex(0).summary is not None:
+        res += f' info={variant_value.GetChildAtIndex(0).summary}'
+    return res
+
+
+def Signature_Summary(valobj, _dict, _options):
+    nonlocals = valobj.GetChildMemberWithName('nonlocals').children
+    param_type = valobj.GetChildMemberWithName('param_type')
+    return_type = valobj.GetChildMemberWithName('return_type')
+    if nonlocals:
+        nonlocals_str = f"{{{', '.join(v.summary for v in nonlocals)}}}"
+    else:
+        nonlocals_str = ""
+    return f"{nonlocals_str}{param_type.summary} -> {return_type.summary}"
+
+
+def SymbolPointer_Summary(valobj, _dict, _options):
+    symtab = valobj.GetChildMemberWithName('m_symtab').Dereference()
+    symidx = valobj.GetChildMemberWithName('m_symidx').GetValueAsUnsigned()
+    symtab_name = symtab.GetChildMemberWithName('m_name')
+    m_symbols = symtab.GetChildMemberWithName('m_symbols')
+    symbols = m_symbols.GetChildAtIndex(0)
+    symbol_type = m_symbols.GetType().template_args[0]
+    sym = symbols.CreateChildAtOffset("", symidx * symbol_type.size, symbol_type)
+    # print(sym)
+    sym_name = sym.GetChildMemberWithName('m_name')
+    return f"{unquote(symtab_name.summary)}::{unquote(sym_name.summary)}"
+
+
 def __lldb_init_module(debugger, dict):
     print("Loading xci::script formatters...")
     debugger.HandleCommand("type synthetic add -x '^xci::core::IndexedMap<' --python-class xci_script_lldb.IndexedMap_SyntheticChildrenProvider --category xci")
     debugger.HandleCommand("type summary add -x '^xci::core::IndexedMap<' --summary-string 'size=${var.m_size}' -w xci")
+    debugger.HandleCommand("type summary add 'xci::script::TypeInfo' --python-function xci_script_lldb.TypeInfo_Summary -w xci")
+    debugger.HandleCommand("type summary add 'xci::script::Signature' --python-function xci_script_lldb.Signature_Summary -w xci")
+    debugger.HandleCommand("type summary add 'xci::script::SymbolPointer' --python-function xci_script_lldb.SymbolPointer_Summary -w xci")
+    debugger.HandleCommand("type summary add xci::script::MatchScore --summary-string 'exact=${var.m_exact}, coerce=${var.m_coerce}, generic=${var.m_generic}' -w xci")
     debugger.HandleCommand("type category enable xci")
 
