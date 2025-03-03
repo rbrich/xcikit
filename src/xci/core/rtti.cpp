906e084da19c52f42d8fbcb1a816bca91fdf37d6
Radek Brich <radek.brich@devl.cz>
2025-03-03 23:18:35 +0100

    [core] Add backtrace

--- src/xci/core/rtti.cpp
+++ src/xci/core/rtti.cpp
@@ -1,7 +1,7 @@
 // rtti.cpp created on 2018-07-03 as part of xcikit project
 // https://github.com/rbrich/xcikit
 //
-// Copyright 2018 Radek Brich
+// Copyright 2018–2025 Radek Brich
 // Licensed under the Apache License, Version 2.0 (see LICENSE file)
 
 #include "rtti.h"
@@ -15,18 +15,28 @@
 namespace xci::core {
 
 
-std::string demangle_type_name(const char* name)
+std::string demangle(const char* name)
 {
 #if defined(__GNUC__) || defined(__clang__)
     int status;
-    char* realname = abi::__cxa_demangle(name, nullptr, nullptr, &status);
+    char* demangled = abi::__cxa_demangle(name, nullptr, nullptr, &status);
     if (status != 0) {
         // failure
         return name;
     }
-    std::string result(realname);
-    std::free(realname);
+    std::string result(demangled);
+    std::free(demangled);
     return result;
+#else
+    return name;
+#endif
+}
+
+
+std::string demangle_type_name(const char* name)
+{
+#if defined(__GNUC__) || defined(__clang__)
+    return demangle(name);
 #else
     // Windows: not mangled
     std::string res(name);
